83484f3ed63158b9a76d7e182274964dd437c63e
Michaelszeng <michaelszeng@gmail.com>
2023-01-15 01:38:13 -0500

    removed gitignore files

--- backend_sanity/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'mkrb0h7p',
-    dataset: 'production'
-  }
-})
--- frontend_react/src/client.js
+++
@@ -1,5 +0,0 @@
-//NOTE: MUST PLACE THIS FILE IN GIT IGNORE
-
-import sanityClient from '@sanity/client'
-import imageUrlBuilder from '@sanity/image-url'
-
